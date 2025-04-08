01fa6fa34cd9fca27117b7cf9350aa34f4fee852
Jashuva-1 <jashuvarayapati1@gmail.com>
2025-04-08 21:59:14 +0530

    Update and rename lab2.txt to lab2.sh

---
+++ lab2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1. List all files in the current directory that start with the letter 'a':
+ls a*
+
+# 2. List all files in the current directory with a .txt extension:
+ls *.txt
+
+# 3. Find all files in the current directory whose names have exactly five characters:
+ls ?????
+
+# 4. List files in the current directory that start with any letter between 'b' and 'e':
+ls [b-e]*
+
+# 5. List files in the current directory ending with .log, where the second character is a number (e.g., a1.log):
+ls ?[0-9].log
+
+# 6. List all files in the current directory whose names start with 'data' followed by a single character (e.g., data1, dataA):
+ls data?
